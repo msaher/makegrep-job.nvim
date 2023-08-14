c2026a3648dd0693346bb507f89c6dd69f67361b
saher <msaher.shair@gmail.com>
2023-08-14 18:26:59 +0300

    feat: run in qflist function

--- lua/makegrep/init.lua
+++ lua/makegrep/init.lua
@@ -20,4 +20,32 @@ local function collect_job_results(cmd, cb)
 
     return vim.fn.jobstart(cmd, opts)
 end
+
+local function run_in_qflist(cmd, efm, loclist)
+    local setlist;
+    local open;
+    if loclist then
+        open = vim.cmd.ll
+        setlist = function(list, action, what)
+            vim.fn.setloclist(0, list, action, what)
+        end
+    else
+        open = vim.cmd.cc
+        setlist = vim.fn.setqflist
+    end
+
+    print(cmd)
+    collect_job_results(cmd, function(results, _)
+        local lines = results[1]
+        table.remove(lines) -- remove last empty line
+
+        setlist({}, 'r', {
+            lines = lines,
+            efm = efm,
+        })
+
+        open()
+    end)
+
+end
 return M
